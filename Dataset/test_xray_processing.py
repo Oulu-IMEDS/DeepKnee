2bdb38315614e41224519d198d8f0a5031d9e9a6
lext <aleksei.tiulpin@protonmail.ch>
2018-08-07 22:17:03 +0300

    improved the testing for a custom dataset

--- Dataset/test_xray_processing.py
+++ Dataset/test_xray_processing.py
@@ -1,21 +1,22 @@
 from xray_processor import process_file
 import os
 import numpy as np
+import argparse
 
 if __name__ == "__main__":
-    DS_DIR = '/mnt/nas/MOST/Images/XR'
-    TO_SAVE = 'KL_dataset/'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", default='../../DICOM_TEST/imgs/')
+    parser.add_argument("--save_dir", default='../../DICOM_TEST/imgs/')
+    parser.add_argument("--detections", default='../../detection_results.txt')
+    args = parser.parse_args()
 
-    detections = np.loadtxt('MOST_train.csv', dtype=str)
-    # Use this file to determine which knee was badly detected
-    # number indicates the line numer in "the array detections"
-    bad_detections = set(np.loadtxt('MOST_train_poor_detections.csv',dtype=str).tolist())
-
-    for i in range(2):
+    os.makedirs(args.save_dir, exist_ok=True)
+    detections = np.loadtxt(args.detections, dtype=str)
+    
+    for i in range(detections.shape[0]):
         fname, bbox = detections[i][0], detections[i][1:].astype(int)
         # read KL grades for this file somewhere
         # ATTENTION: these grades are FAKE and you need to retrieve them depending on the filename
-        gradeL, gradeR = 5, 5
-        process_file(i, fname, DS_DIR, TO_SAVE, bbox, gradeL, gradeR)
+        process_file(i, fname, args.data_dir, args.save_dir, bbox, 5, 5)
         print(fname, bbox)
     
