75af796c3839ce08f6f129a607488369f4b022f3
Egor Panfilov <egor.v.panfilov@gmail.com>
2018-08-31 11:36:01 +0300

    Fix crop RoIs for 1-image datasets

--- Dataset/crop_rois_your_dataset.py
+++ Dataset/crop_rois_your_dataset.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import argparse
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", default='../../DICOM_TEST/imgs/')
@@ -11,12 +12,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     os.makedirs(args.save_dir, exist_ok=True)
-    detections = np.loadtxt(args.detections, dtype=str)
+
+    detections = np.loadtxt(args.detections, dtype=str, ndmin=2)
     
     for i in range(detections.shape[0]):
-        fname, bbox = detections[i][0], detections[i][1:].astype(int)
-        # read KL grades for this file somewhere
-        # ATTENTION: these grades are FAKE and you need to retrieve them depending on the filename
+        fname, bbox = detections[i][0], detections[i][1:].astype(np.int)
+
+        # ATTENTION: the KL grades specified below are FAKE and you need to
+        #            retrieve the correct values by yourself (e.g. extract
+        #            from the filenames or pass from metadata dataframe)
+
         process_file(i, fname, args.data_dir, args.save_dir, bbox, 5, 5)
         print(fname, bbox)
-    
