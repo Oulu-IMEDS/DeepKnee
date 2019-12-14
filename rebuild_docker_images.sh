0fc7ed4a6e30be100c8d6cf86dd7118678922954
lext <aleksei.tiulpin@protonmail.ch>
2019-12-14 21:34:25 +0200

    the images are pushed to dockerhub. Docker compose is not building any images anymore

--- rebuild_docker_images.sh
+++ rebuild_docker_images.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+# This script must be built on a linux machine
+
 # DeepKnee REST microservice images
 docker build -t miptmloulu/deepknee:gpu -f docker/Dockerfile.gpu .
 docker build -t miptmloulu/deepknee:cpu -f docker/Dockerfile.cpu .
-docker push miptmloulu/deepknee:cpu && docker push miptmloulu/deepknee:gpu
+docker build --build-arg REACT_APP_BROKER_PORT=5002 -t miptmloulu/deepknee:ui -f docker/UIDockerfile deepknee-frontend
+docker build -t miptmloulu/deepknee:broker -f docker/BrokerDockerfile deepknee-backend-broker
 
-# Frontend and Backend broker
+# Frontend and Backend
+docker push miptmloulu/deepknee:cpu && docker push miptmloulu/deepknee:gpu
+docker push miptmloulu/deepknee:broker && docker push miptmloulu/deepknee:ui
+
